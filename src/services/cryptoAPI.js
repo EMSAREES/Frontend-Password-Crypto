3b265f52e5397b6497f824b15ecc0e71c572f553
Emilio S. Reyes E. <Lobomategamer@gmail.com>
2025-05-14 18:22:48 -0700

    modifique las variables globales

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,9 +1,7 @@
-import { API_URL_Config, API_KEY_Config } from '../config.js';
-
 
 export default function useCryptoAPI() {
-  const API_URL = API_URL_Config;
-  const API_KEY = API_KEY_Config ;
+  const API_URL = process.env.REACT_APP_API_URL;
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
   const encryptText = async (data) => {
     try {
